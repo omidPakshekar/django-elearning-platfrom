4c9283d9fee830601d2e0a0731472d43e1265e90
omid.pakshekar.est@gmail.com <omid.pakshekar.est@gmail.com>
2022-06-28 21:24:45 +0430

    add Course and Subject model

--- courses/models.py
+++ courses/models.py
@@ -17,4 +17,13 @@ class Subject(models.Model):
 class Course(models.Model):
     owner   = models.ForeignKey(settings.AUTH_USER_MODEL, related_name = 'courses_created', on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
-  
+    title   = models.CharField(max_length=100)
+    slug    = models.SlugField(max_length=200, unique=True)
+    overview= models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    students = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='course_joined', blank=True)
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
