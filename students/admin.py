18305b1ba5ad868bb6244035f2cec7648bcee2f0
omid.pakshekar.est@gmail.com <omid.pakshekar.est@gmail.com>
2022-07-06 18:06:55 +0430

    add customUserAdmin

--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin 
 from .models import CustomeUserModel
 
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin', 'is_staff')
+    search_fields = ('email', 'username')
+    readonly_fields = ('id', 'date_joined', 'last_login')
 
-admin.site.register(CustomeUserModel)
+    filter_horizontal = ()
+    list_filter = () 
+    fieldsets= ()
+
+admin.site.register(CustomeUserModel, CustomUserAdmin)
