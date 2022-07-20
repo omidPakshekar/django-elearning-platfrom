01ef493be4f1a7f1ca8cde4d60f8a7456b9aaf6a
omid.pakshekar.est@gmail.com <omid.pakshekar.est@gmail.com>
2022-07-20 21:27:48 +0430

    add StudentInlineSerializer

--- api/serializer.py
+++ api/serializer.py
@@ -2,7 +2,13 @@
 from rest_framework import serializers
 from courses.models import Course
 
+
+class StudentInlineSerializer(serializers.Serializer):
+    username = serializers.CharField(read_only=True)
+    
 class CourseSeriaLizer(serializers.ModelSerializer):
+    
+    students = StudentInlineSerializer(many=True)
     class Meta:
         model = Course
         fields = "__all__"
