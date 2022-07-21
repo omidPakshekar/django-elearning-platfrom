556a35b0ee375b95126b772690e9f70a112e15f5
omid.pakshekar.est@gmail.com <omid.pakshekar.est@gmail.com>
2022-07-21 19:14:47 +0430

    add ownerPermisssion and chagne serializer

--- api/serializer.py
+++ api/serializer.py
@@ -1,13 +1,20 @@
 
 from rest_framework import serializers
 from courses.models import Course
+from students.models import CustomeUserModel
 
 
 class StudentInlineSerializer(serializers.Serializer):
     username = serializers.CharField(read_only=True)
-    
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomeUserModel
+        fields = ['email']
+
 class CourseListSeriaLizer(serializers.ModelSerializer):
     students = StudentInlineSerializer(many=True)
+    owner = UserSerializer()
     class Meta:
         model = Course
         fields = "__all__"
--- api/views.py
+++ api/views.py
@@ -4,10 +4,11 @@ from rest_framework.response import Response
 from rest_framework import generics, viewsets
 from .serializer import CourseListSeriaLizer, CourseCreateSeriaLizer
 from courses.models import Course
+from .permissions import IsOwnerOrReadOnly
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
-    
+    permission_classes = [IsOwnerOrReadOnly]
     def get_serializer_class(self):
         if self.action in "list":
             return CourseListSeriaLizer
