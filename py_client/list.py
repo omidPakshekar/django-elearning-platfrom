a5d35de7fff4f0ea97db3a46662890379a7b442a
omid.pakshekar.est@gmail.com <omid.pakshekar.est@gmail.com>
2022-07-22 21:05:50 +0430

    add create.py api client

--- py_client/list.py
+++ py_client/list.py
@@ -4,27 +4,34 @@ from getpass import getpass
 
 auth_endpoint = "http://127.0.0.1:8000/api/token/"
 
-# password = getpass()
-# email = input('your email?')
-# data ={
-#     "email" : email,
-#     "password" : password
-# }
-# auth_response = requests.post(auth_endpoint, json=data)
-# print('auth_response=', auth_response.json())
+password = getpass()
+email = input('your email?')
+data ={
+    "email" : email,
+    "password" : password
+}
+auth_response = requests.post(auth_endpoint, json=data)
+print('auth_response=', auth_response.json())
+
 
 endpoint = "http://127.0.0.1:8000/api/course/"
-response = requests.post(endpoint)
-print('response=', response.json())
 
-# if auth_response.status_code == 200:
-#     token = auth_response.json()['token']    
-#     headers = {
-#         "Authorization" : f"Bearer {token}"
-#     }
-#     get_response = requests.get( endpoint, headers=headers) # HTTP request
-#     data = get_response.json()
-    
+if auth_response.status_code == 200:
+    token = auth_response.json()['access']    
+    headers = {
+        "Authorization" : f"Bearer {token}"
+    }
+    data = {
+        "title" : "create with api2",
+        "slug" : "apislug2",
+        "overview" : "it's simple api2222222222222",
+        "owner" : 2,
+        "category" : 2,
+        "students" : [3],
+    }
+    get_response = requests.post( endpoint, headers=headers, json=data) # HTTP request
+    data = get_response.json()
+    print('data=', data)
 
 # auth_endpoint = "http://127.0.0.1:8000/api/course/"
 # # password = getpass()
